4ccb40464c825266e77db89f143ccfb44d64df8d
ChengRanORZ <luorq0533@qq.com>
2024-07-14 07:57:20 +0800

    patch

--- tasks/FindJade/script_task.py
+++ tasks/FindJade/script_task.py
@@ -1,6 +1,7 @@
 import sys
 
 import importlib
+from datetime import datetime,timedelta
 from dev_tools.assets_test import detect_image
 from module.base.utils import load_module
 from module.config.config import Config
@@ -13,7 +14,7 @@ from tasks.GameUi.game_ui import GameUi
 from tasks.GameUi.page import page_main
 from tasks.base_task import BaseTask
 from typing import List
-from module.logger import logger, attrlogger
+from module.logger import logger
 
 
 class ScriptTask(GameUi, FindJadeAssets):
@@ -21,7 +22,10 @@ class ScriptTask(GameUi, FindJadeAssets):
         con = self.config
         self.ui_get_current_page()
         for item in self.parse():
-            logger.info("start %s-%s ")
+            logger.info("start %s-%s ",item[0],item[1])
+            if not self.needLogin(item):
+                logger.warning("%s Skipped last Login Time:%s", item[0], item[4])
+                continue
             suc = SwitchAccount(self.config, self.device, item[0], item[1], item[2], item[3]).switchAccount()
             if not suc:
                 continue
@@ -30,17 +34,27 @@ class ScriptTask(GameUi, FindJadeAssets):
                 method()
             except TaskEnd as e:
                 logger.warning("%s-%s TaskEnd", item[0], item[1])
+                # 更新配置文件中的时间
+                accountInfoList = self.config.find_jade.find_jade_config.find_jade_accounts_info
+                index = [item.character for item in accountInfoList].index(item[0])
+                accountInfoList[index].last_complete_time = datetime.now()
+                self.config.save()
                 continue
 
         pass
 
     def parse(self) -> List[tuple]:
+        """
+            如果后期更改配置文件格式而做的先手准备,
+        @return:
+        @rtype:
+        """
         confList = self.config.find_jade.find_jade_config.find_jade_accounts_info
         if not confList:
             return []
         retList = []
         for item in confList:
-            retList.append((item.character, item.svr, item.account, item.appleOrAndroid))
+            retList.append((item.character, item.svr, item.account, item.appleOrAndroid, item.last_complete_time))
         return retList
 
         # return [('缘神一世', '立秋', '150****2279', True),
@@ -48,6 +62,22 @@ class ScriptTask(GameUi, FindJadeAssets):
         #         ('停摆六世', '立秋', 'luorq05336@163.com', True),
         #         ('缘神一世', '立秋', '150****2279', True),
         #         ]
+
+    def needLogin(self, item: tuple):
+        """
+            根据上次登陆时间 判断是否需要登录查找
+        @param item:
+        @type item:
+        """
+        lastTime = item[4]
+        now = datetime.now()
+        if now - lastTime > timedelta(hours=13):
+            return True
+        if (lastTime.hour >= 18 or lastTime.hour < 5) and (18>now.hour >= 5):
+            return True
+        if (5 <= lastTime.hour < 18) and now.hour >= 18:
+            return True
+        return False
 
     def WantedQuestScript(self):
         module_name = 'script_task'
@@ -61,8 +91,6 @@ class ScriptTask(GameUi, FindJadeAssets):
 
 
 if __name__ == '__main__':
-
-
     from module.config.config import Config
     from module.device.device import Device
 
